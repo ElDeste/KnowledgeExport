de8ad46829fb0bf2a94223b921e5457387ed695a
ElDeste <133285386+ElDeste@users.noreply.github.com>
2023-05-11 16:16:12 -0700

    Add files via upload

---
+++ ReplaceSpaceWithUnderscores.js
@@ -0,0 +1,15 @@
+<!-- HTML -->
+<input type="text" id="107263553" oninput="convertInput()" />
+<input type="text" id="107264558" />
+
+<script>
+  function convertInput() {
+    const inputField = document.getElementById('107263553');
+    const outputField = document.getElementById('107264558');
+    
+    const inputValue = inputField.value;
+    const convertedValue = inputValue.replace(/\s+/g, '_'); // Replace spaces with underscores
+    
+    outputField.value = convertedValue;
+  }
+</script>
